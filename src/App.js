4328550ea45d3b4e9e8da260a13aaf79dc84c82a
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-04-12 09:58:36 -0700

    Updated links to textures

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App () {
       <Scene>
         <a-assets>
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" alt="ground texture"/>
-          <img id="animeGrass1" src="https://cdn.aframe.io/a-painter/images/floor.jpg" alt="dark anime grass texture"/>
-          <img id="grasslands" src="https://cdn.aframe.io/a-painter/images/floor.jpg" alt="light grasslands"/>
+          <img id="animeGrass1" src="https://cdn.glitch.global/c76a4fd7-d4e0-495b-a83b-9db94de330a2/animeGrass1.jpeg?v=1681247445096" alt="dark anime grass texture"/>
+          <img id="grasslands" src="https://cdn.glitch.global/c76a4fd7-d4e0-495b-a83b-9db94de330a2/grassLand2.jpeg?v=1681318434684" alt="light grasslands"/>
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" alt="sky texture"/>
         </a-assets>
         <Entity id="floor" primitive="a-plane" rotation="-90 0 0" height="120" width="120" src="#animeGrass1"></Entity>
